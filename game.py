611d1740a2d6614c68798ee91dd9f19bb0710b8c
Chase Richards <chaserichards@mail.weber.edu>
2025-01-30 16:49:20 -0700

    Add: Response to player's deciscion

--- game.py
+++ game.py
@@ -33,3 +33,12 @@ print(starting_area)
 # Ask the player for their first decision
 decision = input("Do you wish to take the path? (yes or no):").lower ()
 
+# Respond based on the player's decision
+if decision == "yes":
+    print(f"Brave choice,
+{player_name}! you step onto the path and venture forward.")
+elif decision == "no":
+    print(player_name + ", you decide to wait. perhaps courage will find you later.")
+# Concatenation example
+else:
+    print("Confused, you stand still, unsure of what to do.")
